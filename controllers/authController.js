7bdc041a6df54e443813fe94c94c802bbe223e96
Victor <vpradosaraiva@gmail.com>
2026-08-14 08:51:25 -0300

    implementa hash de senha

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@
 
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require ("bcrypt");
 
 exports.register = async (req, res) => {
 
@@ -24,11 +25,11 @@ exports.register = async (req, res) => {
             });
 
         }
-
+const passwordHash = await bcrypt.hash (password, 10)
         const user = await User.create({
             name,
             email,
-            password
+            password: passwordHash
         });
 
         return res.status(201).json({
@@ -63,10 +64,15 @@ exports.login = async (req, res) => {
         const user = await User.findOne({
             email
         });
+ 
+        const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
 
         if (
             !user ||
-            user.password !== password
+            passwordMatch
         ) {
 
             return res.status(401).json({
